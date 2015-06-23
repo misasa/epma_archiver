b8570d56033ebb5d605cb00655b052ef4e37df43
Yusuke Yachi <yusuke.yachi@gmail.com>
2015-06-23 10:14:03 +0900

    yy

--- config/schedule.rb
+++ config/schedule.rb
@@ -18,7 +18,7 @@
 # end
 
 # Learn more: http://github.com/javan/whenever
-set :output, {:error => 'log/error.log', :standard => 'log/crontab.log'}
+set :output, {:error => 'log/crontab_error.log', :standard => 'log/crontab.log'}
 set :environment, :production
 
 every '*/5 * * * *' do
