341be1d26a0ac3240b4809a685e5cefc516b5ee9
Yusuke Yachi <yusuke.yachi@gmail.com>
2016-04-23 11:19:43 +0900

    map top message rm within 1 hour link

--- app/helpers/areas_helper.rb
+++ app/helpers/areas_helper.rb
@@ -5,10 +5,9 @@ module AreasHelper
 	  	msgs << "This page summarizes a map analysis"
   		msgs << "on an area (#{@area.name})" if @area.name
   		msgs[-1] += "."
-  		now_link = link_to("now",job_area_path(@area),:method => 'post',:data => { :confirm => "Are you sure you want to process #{@area.name} now?" })
-  		#delete_link = link_to("within 1 hour",job_area_path(@area),:method => 'post',:data => { :confirm => "Are you sure you want to process #{@area.name} within 1 hour?" })
-		delete_link = link_to("within 1 hour",area_path(@area),:method => 'delete',:data => { :confirm => "Are you sure you want to process #{@area.name} within 1 hour?" })
-  		msgs << "Please download " + link_to("files", asset_path(@area.zip.url)) + " or reprocess " + now_link + " | " + delete_link + "."
+  		now_link = link_to("reprocess",job_area_path(@area),:method => 'post',:data => { :confirm => "Are you sure you want to process #{@area.name}?" })
+		#delete_link = link_to("within 1 hour",area_path(@area),:method => 'delete',:data => { :confirm => "Are you sure you want to process #{@area.name} within 1 hour?" })
+  		msgs << "Please download " + link_to("files", asset_path(@area.zip.url)) + " or " + now_link + "."
   	else
   		msgs << "Processing ..."
   	end
