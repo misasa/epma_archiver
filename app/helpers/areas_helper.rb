925d74a1335b5f43b12e15a85206fb48dd56ee0c
Yusuke Yachi <yusuke.yachi@gmail.com>
2016-04-25 10:36:04 +0900

    change top message of map analysis page

--- app/helpers/areas_helper.rb
+++ app/helpers/areas_helper.rb
@@ -1,15 +1,15 @@
 module AreasHelper
   def top_message
   	msgs = []
+	msgs << "A map analysis"
   	if @area.zip.path
-	  	msgs << "This page summarizes a map analysis"
   		msgs << "on an area (#{@area.name})" if @area.name
   		msgs[-1] += "."
   		now_link = link_to("reprocess",job_area_path(@area),:method => 'post',:data => { :confirm => "Are you sure you want to process #{@area.name}?" })
 		#delete_link = link_to("within 1 hour",area_path(@area),:method => 'delete',:data => { :confirm => "Are you sure you want to process #{@area.name} within 1 hour?" })
-  		msgs << "Please download " + link_to("files", asset_path(@area.zip.url)) + " or " + now_link + "."
+  		msgs << "Download " + link_to("files", asset_path(@area.zip.url)) + " or " + now_link + "."
   	else
-  		msgs << "Processing ..."
+  		msgs << "is under process. Reload occasionally. If process takes > 1 hour, contact administrator."
   	end
   	raw(msgs.join(" "))
 
